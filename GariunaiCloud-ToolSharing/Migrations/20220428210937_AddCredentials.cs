0d4f408d90e2e8c0215e41ce1ed912cbbe614ccb
Martynas Cibulskis <martynascibulskis6@gmail.com>
2022-04-29 01:19:38 +0300

    Add missing files

---
+++ GariunaiCloud-ToolSharing/Migrations/20220428210937_AddCredentials.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GariunaiCloud_ToolSharing.Migrations
+{
+    public partial class AddCredentials : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Listings",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Listings",
+                keyColumn: "ListingId",
+                keyValue: 1L,
+                column: "City",
+                value: "Klaipėda");
+
+            migrationBuilder.UpdateData(
+                table: "Listings",
+                keyColumn: "ListingId",
+                keyValue: 2L,
+                column: "City",
+                value: "Vilnius");
+
+            migrationBuilder.UpdateData(
+                table: "Listings",
+                keyColumn: "ListingId",
+                keyValue: 3L,
+                column: "City",
+                value: "Kaunas");
+
+            migrationBuilder.UpdateData(
+                table: "Listings",
+                keyColumn: "ListingId",
+                keyValue: 4L,
+                column: "City",
+                value: "Balbieriškis");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Listings");
+        }
+    }
+}
