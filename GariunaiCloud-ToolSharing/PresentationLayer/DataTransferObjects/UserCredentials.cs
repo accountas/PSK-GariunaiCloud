e7958b30e34f38913db6b7e692bba8327fc02d81
Martynas <55199871+accountas@users.noreply.github.com>
2022-05-01 19:02:41 +0300

    Fix required properties in user credentials dto

--- GariunaiCloud-ToolSharing/PresentationLayer/DataTransferObjects/UserCredentials.cs
+++ GariunaiCloud-ToolSharing/PresentationLayer/DataTransferObjects/UserCredentials.cs
@@ -5,10 +5,8 @@ namespace GariunaiCloud_ToolSharing.PresentationLayer.DataTransferObjects;
 #nullable disable
 public class UserCredentials
 {
-    [Required]
     public string Username { get; set; }
 
-    [Required] 
     public string Email { get; set; }
 
     [Required]
